079e2edacb372705cb9fee579b88db01ba2fa425
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-07-23 13:41:57 +0900

    ugokuka wakaran

---
+++ src/routes/CalcTypeSelect.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { calcTypes } from "./store.js";
+    export let selected;
+</script>
+
+<select bind:value={selected}>
+    {#each $calcTypes as calcType}
+        <option value={calcType}>{calcType.label}</option>
+    {/each}
+</select>
+
+<style>
+    select {
+        background: #1f2937;
+    }
+</style>
---
+++ src/routes/Settings/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Form from "./Form.svelte";
+    import Table from "./Table.svelte";
+    import { calcTypes } from "../store.js";
+</script>
+
+<div>
+    {#each $calcTypes as { label, rates }, index}
+        <Table {label} {rates} {index} />
+    {/each}
+</div>
+
+<Form />
+
+<style>
+    div {
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
